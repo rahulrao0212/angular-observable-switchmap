55dc842b6ddfc166381a8d0f02ce8ef4d9c8f97c
Rahul Rao <RAOR378647@exactsoftware.com>
2020-12-17 14:12:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, interval, Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'angular-observable-switchmap';
+
+  //inner observable example
+  srcObservable = of(1, 2, 3, 4)
+  innerObservable = of('A', 'B', 'C', 'D')
+  obs = of(1, 2, 3, 4)
+
+  ngOnInit() {
+    this.srcObservable.pipe(
+      switchMap(val => {
+        console.log('Source value ' + val)
+        console.log('starting new observable')
+        return this.innerObservable
+      })
+    )
+      .subscribe(ret => {
+        console.log('Recd ' + ret);
+      })
+
+    this.obs.pipe(
+      switchMap(val => {
+        return of(val * 2)  //Returning observable
+      })
+    )
+      .subscribe(ret => {
+        console.log('Recd from switchMap : ' + ret);
+      })
+  }
+
+  //button example
+  @ViewChild('btn', { static: true }) button: ElementRef;
+  clicks$: Observable<any>;
+
+  ngAfterViewInit() {
+    this.clicks$ = fromEvent(this.button.nativeElement, 'click');
+    this.switchExample();
+  }
+
+  switchExample() {
+    this.clicks$
+      .pipe(
+        switchMap(() => {
+          return interval(500)
+        })
+      )
+      .subscribe(val => console.log(val));
+  }
 }
